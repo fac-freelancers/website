cacff743fae6d2f082b188d57c7cb5029d717161
shouston3 <shouston3@me.com>
2016-06-16 17:20:14 +0100

    Adding an enlarged class to the lightbulb symbol (first svgs element). Adding howWeWork js file to change the content and change the size of the icons

---
+++ js/howWeWork.js
@@ -0,0 +1,32 @@
+const howWeWorkIcons = document.getElementsByClassName('howWeWorkEl');
+const icons = Array.from(howWeWorkIcons);
+
+const howWeWorkCont = document.getElementsByClassName('howWeWorkContent');
+const cont = Array.from(howWeWorkCont);
+
+const howWeWorkSvgs = document.getElementsByClassName('howWeWork-elements');
+const svgs = Array.from(howWeWorkSvgs);
+
+function changeContent(i) {
+  cont.forEach(function (c) {
+    if(Array.from(c.classList).indexOf('hidden') === -1)
+      c.classList.add('hidden');
+  });
+  cont[i].classList.remove('hidden');
+}
+
+function enlargeIcon(i) {
+  svgs.forEach(function (s) {
+    const iEnlarged = Array.from(s.classList).indexOf('enlarged');
+    if(iEnlarged !== -1) s.classList.remove('enlarged');
+  });
+  svgs[i].classList.add('enlarged');
+}
+
+icons.forEach(function (icon, i) {
+  icon.addEventListener('click', function () {
+    changeContent(i);
+    enlargeIcon(i);
+  });
+});
+
