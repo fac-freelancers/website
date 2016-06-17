a66e54a8210703ab5f0fe0b4339022d540ad5574
shouston3 <shouston3@me.com>
2016-06-17 15:49:50 +0100

    Instead of using the section tag added a class of section so that I can use the section tag without it inheriting the section css properties

--- _harp/public/js/page-size.js
+++ _harp/public/js/page-size.js
@@ -2,7 +2,7 @@
   const navHeight = document.getElementsByTagName('nav')[0].clientHeight;
   const windHeight = window.innerHeight;
 
-  const sections = Array.from(document.getElementsByTagName('section'));
+  const sections = Array.from(document.getElementsByClassName('section'));
 
   sections[0].style.marginTop = navHeight + 'px';
 
