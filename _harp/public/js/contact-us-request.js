8ca8605498508cf732bc6f0afeb074f2b58a5814
shouston3 <shouston3@me.com>
2016-06-18 13:02:09 +0100

    Refactoring contact-us-request.js to now send a get request

--- _harp/public/js/contact-us-request.js
+++ _harp/public/js/contact-us-request.js
@@ -1,27 +1,25 @@
 (function () {
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('email').value;
-  var website = document.getElementById('website').value;
-  var budget = document.getElementById('budget').value;
-  var message = document.getElementById('message').value;
+  var info = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    website: document.getElementById('website').value,
+    budget: document.getElementById('budget').value,
+    message: document.getElementById('message').value
+  };
+
+  console.log(info.name, info.email, info.website, info.budget, info.message);
+
   var button = document.getElementById('sendEmail');
 
   button.addEventListener('click', function (e) {
     e.preventDefault();
-    console.log('submitted with: ', name, email, website, budget, message);
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
       if(xhr.readyState === 4 && xhr.status === 200) {
         console.log('response is: ' + xhr.responseText);
       }
     };
-    xhr.open('post', 'http://factree-api.herokuapp.com/mail');
-    xhr.send(JSON.stringify({
-      name: name,
-      email: email,
-      website: website,
-      budget: budget,
-      message: message
-    }));
+    xhr.open('get', 'http://factree-api.herokuapp.com/mail/' + JSON.stringify(info));
+    xhr.send();
   });
 }());
