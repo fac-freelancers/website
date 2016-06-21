fdeab7da5567f81828f217b9cd9eb8106df350d5
shouston3 <shouston3@me.com>
2016-06-21 17:37:48 +0100

    Dynamically setting the svg heights using js and making them responsive to clicks.

--- _harp/public/js/howWeWork.js
+++ _harp/public/js/howWeWork.js
@@ -1,9 +1,16 @@
 (function () {
   var howWeWorkIcons = document.getElementsByClassName('howWeWorkEl');
-
   var howWeWorkCont = document.getElementsByClassName('howWeWorkContent');
-
   var howWeWorkSvgs = document.getElementsByClassName('howWeWork-elements');
+  var iconHeight = 1 / 8 * (
+    document.getElementsByClassName('section')[0].clientHeight -
+    document.getElementById('howWeWork-title').style.height -
+    2 * document.getElementById('howWeWork-title').style.marginTop
+  );
+  var enlargedIconHeight = 5 / 3 * iconHeight;
+  function widthFromHeight(height) {
+    return 108 / 101 * height; 
+  }
 
   function changeContent(i) {
     [].forEach.call(howWeWorkCont, function (cont) {
@@ -18,9 +25,19 @@
     [].forEach.call(howWeWorkSvgs, function (svg) {
       if(svg.classList.contains('enlarged')) {
         svg.classList.remove('enlarged');
+        svg.style.height = iconHeight;
+        svg.style.width = widthFromHeight(iconHeight);
       }
     });
+    [].forEach.call(howWeWorkIcons, function (icon) {
+      if(icon.classList.contains('enlargedWrapper')) {
+        icon.classList.remove('enlargedWrapper');
+      }
+    });
+    howWeWorkIcons[i].classList.add('enlargedWrapper');
     howWeWorkSvgs[i].classList.add('enlarged');
+    howWeWorkSvgs[i].style.height = enlargedIconHeight;
+    howWeWorkSvgs[i].style.width = widthFromHeight(enlargedIconHeight);
   }
 
   [].forEach.call(howWeWorkIcons, function (icon, i) {
@@ -29,4 +46,14 @@
       enlargeIcon(i);
     });
   });
+
+  [].forEach.call(howWeWorkIcons, function (icon) {
+    icon.style.height = iconHeight + 'px';
+    icon.style.width = widthFromHeight(iconHeight) + 'px';
+  });
+
+  howWeWorkIcons[0].classList.add('enlargedWrapper');
+  howWeWorkSvgs[0].classList.add('enlarged');
+  howWeWorkSvgs[0].style.height = enlargedIconHeight + 'px';
+  howWeWorkSvgs[0].style.width = widthFromHeight(enlargedIconHeight) + 'px';
 }());
