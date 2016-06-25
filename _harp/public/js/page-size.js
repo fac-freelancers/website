893628a990f568a1a4a5b5194787ec247315853c
shouston3 <shouston3@me.com>
2016-06-25 22:10:45 +0100

    Correcting linting errors

--- _harp/public/js/page-size.js
+++ _harp/public/js/page-size.js
@@ -1,4 +1,5 @@
 (function () {
+  'use strict';
   var navHeight = document.getElementsByTagName('nav')[0].clientHeight;
   var footerHeight = document.getElementsByTagName('footer')[0].clientHeight;
   var windowHeight = window.innerHeight;
@@ -8,8 +9,8 @@
   sections[0].style.marginTop = navHeight + 'px';
 
   [].forEach.call(sections, function (el) {
-    el.style.height = (windowHeight - navHeight) + 'px';
+    el.style.height = windowHeight - navHeight + 'px';
   });
 
-  sections[sections.length - 1].style.height = (windowHeight - navHeight - footerHeight) + 'px';
-}());
+  sections[sections.length - 1].style.height = windowHeight - navHeight - footerHeight + 'px';
+})();
